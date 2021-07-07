2eac0ed7774f6bd94fe332b7f6b7ee679667dc64
Mohamed Bakayoko <mowamedbakus@gmail.com>
2021-07-07 12:19:50 +0000

    Add stub expense item

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import './Card.css';
 
-const Card = (props: any) => {
-    const classes = 'card ' + props.className;
+const Card = () => {
     return (
-        <div className={classes}>
-            {props.children}
+        <div>
+            
         </div>
     );
 };
---
+++ src/components/ExpenseItem.tsx
@@ -0,0 +1,17 @@
+import './ExpenseItem.css';
+import ExpenseDate from "./ExpenseDate";
+
+const ExpenseItem = (props: any) => {
+
+    return (
+        <div className="expense-item">
+            <ExpenseDate date={props.expense.date} />
+            <div className="expense-item__description">
+                <h2>{props.expense.title}</h2>
+                <div className="expense-item__price">${props.expense.amount}</div>
+            </div>
+        </div>
+    );
+};
+
+export default ExpenseItem;
